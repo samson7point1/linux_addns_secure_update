509a663e27a07c076e2e3d8355e9a2876deebffd
samson7point1 <samson7point1@yahoo.com>
2017-04-18 14:29:34 -0500

    Create create_tracking_dbtable.sql

---
+++ windows_server/inetpub/wwwroot/dnsapi/v1/create_tracking_dbtable.sql
@@ -0,0 +1,29 @@
+USE [AutomationDB]
+GO
+
+/****** Object:  Table [dbo].[dnsapi] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[dnsapi](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[date] [datetime] NULL,
+	[user] [varchar](50) NULL,
+	[ip] [varchar](50) NULL,
+	[action] [varchar](max) NULL,
+	[request] [varchar](max) NULL,
+	[response] [varchar](max) NULL,
+	[command] [varchar](max) NULL,
+	[runtime] [numeric](30, 12) NULL
+)
+
+GO
+
+SET ANSI_PADDING OFF
+GO
